936b725a9919d5037380de2503e02ad4ce637cdf
Raymond4578 <77660950+Raymond4578@users.noreply.github.com>
2022-04-09 00:03:11 +1000

    Add files via upload
    A py to generate csv data.

---
+++ data_recording_function.py
@@ -0,0 +1,13 @@
+import csv
+import os
+
+file_exist = os.path.isfile("SH_COVID_data.csv")
+print(file_exist)
+if file_exist == False:
+    with open('SH_COVID_data.csv','w') as file:
+    writer = csv.writer(file)
+    write.writerow(['record_id', 'date', 'case_CT', 'case_NOCT', 'total'])
+elif file_exist == True:
+    pass
+else:
+    print('Cannot identify whether the file "SH_COVID_data.csv" exists.')
