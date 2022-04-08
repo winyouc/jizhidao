906612c07ce9e4feffdc2f35ba1ea8fad8f73696
winyouc <2368548610@qq.com>
2022-04-08 22:40:25 +0800

    1

--- data_recording_function.py
+++ data_recording_function.py
@@ -5,9 +5,11 @@ file_exist = os.path.isfile("SH_COVID_data.csv")
 print(file_exist)
 if file_exist == False:
     with open('SH_COVID_data.csv','w') as file:
-    writer = csv.writer(file)
-    write.writerow(['record_id', 'date', 'case_CT', 'case_NOCT', 'total'])
+        writer = csv.writer(file)
+        writer.writerow(['record_id', 'date', 'case_CT', 'case_NOCT', 'total'])
 elif file_exist == True:
     pass
 else:
     print('Cannot identify whether the file "SH_COVID_data.csv" exists.')
+
+
